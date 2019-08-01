77a61ba458c843efb438fac3254b89acdc0991da
bharathingowda <bharathigowda20@gmail.com>
2019-08-01 22:54:54 +0530

    Add files via upload
    redirect with react and php

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+          <label>
+            REDIRECT PAGE
+            
+          </label>
+
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
